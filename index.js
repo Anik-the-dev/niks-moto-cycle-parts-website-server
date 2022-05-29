b14cc3eb88fc17faf7d0d7056c00d321e487cb76
Anik-the-dev <niksctg00@gmail.com>
2022-05-29 11:48:17 +0600

    access token create

--- index.js
+++ index.js
@@ -10,24 +10,56 @@ require('dotenv').config();
 const port = process.env.PORT || 4000;
 
 // connect to mongo
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aerj3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aerj3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // verify JWT
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ message: 'UnAuthorized access. Please Login or Register' });
+        return res.status(401).send({ message: 'UnAuthorized access.' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            return res.status(403).send({ message: 'Forbidden access, Please Login or Register' })
+            return res.status(403).send({ message: 'Forbidden access' })
         }
         req.decoded = decoded;
         next();
     });
 }
+
+// connect the database table
+// data CRUD...............
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('niksmotoDB').collection('partsCollection');
+   
+
+        // get data from mongo db
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = partsCollection.find(query)
+            const db = await cursor.toArray()
+            res.send(db)
+        })
+
+        // get data via params
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: objectId(id) }
+            const cursor = partsCollection.find(query)
+            const db = await cursor.toArray()
+            res.send(db)
+        })
+
+    } finally {
+
+    }
+
+}
+run().catch(console.dir);
 
 // check the server..............
 app.get('/', (req, res) => {
