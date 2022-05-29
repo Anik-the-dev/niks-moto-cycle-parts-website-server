ecb97227eb22e33de72899b1f2899b1615b9639e
Anik-the-dev <niksctg00@gmail.com>
2022-05-29 11:32:21 +0600

    jwt verification function done

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const port = process.env.PORT || 4000;
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aerj3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify JWT
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access. Please Login or Register' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access, Please Login or Register' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 // check the server..............
 app.get('/', (req, res) => {
     res.send("Running niksmoto!")
