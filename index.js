376997ea278a8e695b9cee87af3afaacba79a0f9
Anik-the-dev <niksctg00@gmail.com>
2022-05-29 10:39:51 +0600

    package.json created

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ const port = process.env.PORT || 4000;
 // connect to mongo
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aerj3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// check the server..............
+app.get('/', (req, res) => {
+    res.send("Running niksmoto!")
+})
+
+app.listen(port, () => {
+    console.log("Listening port 4000")
+})
