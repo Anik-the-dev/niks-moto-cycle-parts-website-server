8c56b35cdf51122cc04c620e41d67664ebdb66d7
Anik-the-dev <niksctg00@gmail.com>
2022-05-29 18:05:12 +0600

    review api create

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ async function run() {
         app.get('/products', async (req, res) => {
             const query = {}
             const cursor = partsCollection.find(query)
-            const db = await cursor.toArray()
-            res.send(db)
+            const part = await cursor.toArray()
+            res.send(part)
         })
 
         // get data via params
@@ -53,15 +53,15 @@ async function run() {
             const id = req.params.id
             const query = { _id: objectId(id) }
             const cursor = partsCollection.find(query)
-            const db = await cursor.toArray()
-            res.send(db)
+            const part = await cursor.toArray()
+            res.send(part)
         })
 
 
         // post data to Mongo DB
         app.post('/products', async (req, res) => {
             const product = req.body
-            const result = await database.insertOne(product)
+            const result = await partsCollection.insertOne(product)
             res.send({ ack: "product added to server" })
         })
 
@@ -98,9 +98,82 @@ async function run() {
             res.send(result)
         })
 
-        // users
+        // users api
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.post('/user', async (req, res) => {
+            const p = req.body
+            const result = await userCollection.insertOne(p)
+            res.send({ ack: "Profile Update" })
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+
+        // delete a single User
+        app.delete('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // admin api
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
 
         // review
+
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewCollection.find(query)
+            const rev = await cursor.toArray()
+            res.send(rev)
+        })
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body
+            const result = await reviewCollection.insertOne(review)
+            res.send({ ack: "review added to server" })
+        })
 
     } finally {
 
