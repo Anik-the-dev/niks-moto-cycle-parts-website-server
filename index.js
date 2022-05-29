ff0d835bc3be47ecd354fcef4d262b9ec7a823c9
Anik-the-dev <niksctg00@gmail.com>
2022-05-29 10:37:13 +0600

    connected to the database

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config();
 const port = process.env.PORT || 4000;
+
+// connect to mongo
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aerj3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
